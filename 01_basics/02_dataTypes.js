928499ad4dd4548e3f09303f717f76e788810f6c
Furqan Ahmed <156416600+superhackerdon@users.noreply.github.com>
2026-03-30 12:46:28 +0000

    dattypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"
+// It means we are using new syntax with respect to newer version of js as many things were not there in older js version
+// Although now a days this is automatically done in js it is still a good practice to write this line
+// once used in a file u cannot use in some part older version and in some part newer version of js
+// alert( 3 + 3) // we are using node js not browser
+console.log(3+3);
+//ecma script is the most recognised official documentation of js 
+// mdn is another such documentation
+// This is called specification which is not neened for you
+
+let name = "Ahmed" //string
+let age = 22 //number
+let isLoggedIn = false //boolean
+
+//string "" or ''
+//number => aroung 2 to power 53 and bigint also avl
+// boolean => true/false
+
+// null is datatype and also standalone value
+// undefined 
+// null is special type which is empty and undefined is when variable is defined but value is not assigned
+// symbol used for uniqueness
+// PRIMITIVE DATATYPES OVER
+
+//object
+
+console.log(typeof age);
+console.log(typeof undefined); // output is undefined
+console.log(typeof null); // output is object
+// undefined apne aap me ek type hai lekin null ek object hai

e39e86ad571c197b03cf6fcf3f5d8a4408bfa383
Furqan Ahmed <156416600+superhackerdon@users.noreply.github.com>
2026-03-31 11:15:55 +0000

    Type of learnt

--- 01_basics/03_coversionOperation.js
+++ 01_basics/03_coversionOperation.js
@@ -11,6 +11,7 @@ console.log(typeof score1);
 let valueInNumber = Number(score1)
 // console.log(typeof score1); this was written by mistake by me dont do this again
 console.log(typeof valueInNumber);
+// converted to number
 
 // Now  another scenario
 let score2 = "22abc"
@@ -20,3 +21,25 @@ let valInNumber = Number(score2)
 console.log(typeof valInNumber);
 // output is number but abc shouldnt have converted to number
 console.log(valInNumber);
+// output is NaN indicating not a number
+
+
+let score3 = null
+console.log(typeof score3);
+let val3InNumber = Number(score3)
+console.log(typeof val3InNumber);
+console.log(val3InNumber);
+
+
+let score4 = undefined
+console.log(typeof score4);
+let val4InNumber = Number(score4)
+console.log(typeof val4InNumber);
+console.log(val4InNumber);
+
+let score5 = undefined
+console.log(typeof score5);
+let val5InNumber = Number(score5)
+console.log(typeof val5InNumber);
+console.log(val5InNumber);
+

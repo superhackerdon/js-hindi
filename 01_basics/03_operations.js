ca939cf93cfeaf9e6ddaa6b99a5a7acf457dde58
Furqan Ahmed <156416600+superhackerdon@users.noreply.github.com>
2026-03-31 13:01:39 +0000

    some operations

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,58 @@
+let value = 3
+let neg , negvalue = -value
+console.log(negvalue);
+// output
+// undefined
+// -3
+// neg , wasadded by me
+
+// Arithematic oprations
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// **(power), /, % also available
+
+// exam point view some questions
+
+let str1 = "Furqan"
+let str2= "Ahmed"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// output is 12
+
+// console.log(1 + "2");
+// output is 12
+
+// console.log("1" + 2 + 2);
+// output is 122
+
+// console.log(1 + 2 + "2");
+// output is 32
+// reason of these output is that it is made like this syntax!
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// means you are incrementing the value true+ not accepted
+// output is 1 as true can only be incremented to 1
+
+// console.log(+"");
+// "" for this ans is false so with + converted to 0
+// output is zero
+
+// let num1,num2,num3
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+// gamecounter++; this is post increment
+// ++gameCounter; this is pre increment
+// console.log(gameCounter);
+// or to get the value of game counter after increment we can do like this
+console.log(gameCounter++);
+// output is 100 because of post increment
+// if we want to get 101 then we have to use pre increment
+// console.log(++gameCounter);
+// output is 101
+

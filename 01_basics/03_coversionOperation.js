3d15357a2249df61a292ba07a420702dc2ee40de
Furqan Ahmed <156416600+superhackerdon@users.noreply.github.com>
2026-03-31 12:27:40 +0000

    Conversion of data types

--- 01_basics/03_coversionOperation.js
+++ 01_basics/03_coversionOperation.js
@@ -21,7 +21,10 @@ let valInNumber = Number(score2)
 console.log(typeof valInNumber);
 // output is number but abc shouldnt have converted to number
 console.log(valInNumber);
-// output is NaN indicating not a number
+// output is 
+// string
+// number
+// NaN indicating not a number
 
 
 let score3 = null
@@ -29,17 +32,45 @@ console.log(typeof score3);
 let val3InNumber = Number(score3)
 console.log(typeof val3InNumber);
 console.log(val3InNumber);
-
+// output
+// object
+// number
+// 0
 
 let score4 = undefined
 console.log(typeof score4);
 let val4InNumber = Number(score4)
 console.log(typeof val4InNumber);
 console.log(val4InNumber);
+// output
+// undefined
+// number so, NaN type is number
+// NaN
 
-let score5 = undefined
+let score5 = true
 console.log(typeof score5);
 let val5InNumber = Number(score5)
 console.log(typeof val5InNumber);
 console.log(val5InNumber);
+// output
+// boolean
+// number
+// 1
 
+let score6 = 1
+let val6InNumber = Boolean(score6)
+console.log(val6InNumber);
+// output is true
+// Alternate
+// if score 6 is
+// 1 => true; 0 => false
+// "Ahmed" => true
+// "" => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+// output
+// 33
+// string

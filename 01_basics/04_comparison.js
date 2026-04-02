488f211a639e3a37eba23c4cae32a6501c246b77
Furqan Ahmed <156416600+superhackerdon@users.noreply.github.com>
2026-04-02 06:32:53 +0000

    comparision

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2>=1);
+// <, !=, ==,  also possible
+
+console.log("2">1);
+console.log("02">1);
+// output
+// true
+// true
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// output
+
+

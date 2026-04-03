3a8e789eb71e4394696f93aaf01ad8610e4bbe1c
Furqan Ahmed <156416600+superhackerdon@users.noreply.github.com>
2026-04-03 11:32:20 +0000

    comparision

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -7,10 +7,30 @@ console.log("02">1);
 // output
 // true
 // true
+// javascript automatically converts string into number
 
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
 // output
+// false as zero is empty
+// false as zero is empty
+// true is because of value conversion i.e as equality and comparisions work differently 
+// equality/equalities are == , === and comparisions are >, <, >=, <=
+// so null == 0 ,empty is not equal to zero
+// comparisions convert null to a number and treat it as zero hence null >=0 is true and null >0 is false
+// null >0 zero is not greater than zero
+// null >=0 zero is not greater than zero yes but equal to zero so condition satisfied
+// this all is defined in mdn we cant do anything
 
+// === (Strict check)
+// It checks strictly like both the data types should be same
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
 
